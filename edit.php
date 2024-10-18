7c5f70bc27ae79c506c511d1b58626534248fb7a
Nikkodaquiuag <167621270+Nikkodaquiuag@users.noreply.github.com>
2024-10-18 16:42:32 +0800

    edit.php

--- edit.php
+++ edit.php
@@ -22,12 +22,12 @@
 </head>
 <body>
 	<?php $getlibrarianByID = getlibrarianByID($pdo, $_GET['lib_id']); ?>
-	<form action="core/handleForms.php" method="POST">
+	<form action="core/handleForms.php?lib_id=<?php echo $_GET['lib_id']; ?>" method="POST">
         <p><label for="names">Name</label> <input type="text" name="names"value="<?php echo $getlibrarianByID['names']; ?>"></p>
 		<p><label for="age">Age</label> <input type="text" name="age"value="<?php echo $getlibrarianByID['age']; ?>"></p>
 		<p><label for="fave_book">Favorite Book</label> <input type="text" name="fave_book"value="<?php echo $getlibrarianByID['fave_book']; ?>"></p>
 		<p><label for="fave_genre">Favorite Genre</label> <input type="text" name="fave_genre"value="<?php echo $getlibrarianByID['fave_genre']; ?>"></p>
-		<p><label for="degree">Degree</label> <input type="text" name="Degree"value="<?php echo $getlibrarianByID['degree']; ?>"></p>
+		<p><label for="degree">Degree</label> <input type="text" name="degree"value="<?php echo $getlibrarianByID['degree']; ?>"></p>
 		<p><label for="experience">Experience</label> <input type="text" name="experience"value="<?php echo $getlibrarianByID['experience']; ?>"></p>
 			<input type="submit" name="editbtn">
 		</p>
